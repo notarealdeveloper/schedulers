452eaebaa06c50becc754b79e038e24e26487ed0
Jason Wilkes <notarealdeveloper@gmail.com>
2024-03-14 05:40:02 -0700

    W||K Tutorial: Awaitables (almost) from scratch

--- src/schedulers/main.py
+++ src/schedulers/main.py
@@ -1,28 +1,41 @@
 #!/usr/bin/env python3
 
+import sys
 import asyncio
 
 # ==========
 
 # this code is inside the library
 
-def retry(coro, times=float('inf')):
-    n = 0
-    while n < times:
+@types.coroutine
+def suspend():
+    yield
+
+
+async def retry(func, *args, **kwds):
+    while True:
         try:
+            coro = func(*args, **kwds)
             return await coro
         except:
-            n += 1
+            await suspend()
+            print(f"error: {coro.__name__}(args={args}, kwds={kwds})", file=sys.stderr)
             continue
 
 # ==========
 
 # this code is outside the library
 
-async def failer(n):
+async def fail(n):
     """ sometimes tasks fail """
     import random
     if random.random() < 0.99:
         raise RuntimeError("no")
+    return n
 
-# ==========
+async def main():
+    coros = [retry(fail, n) for n in range(100)]
+    return await asyncio.gather(*coros)
+
+results = asyncio.run(main())
+
