bca60d3c3511fe3ca17ddd571d6693461d93a0e7
Jason Wilkes <notarealdeveloper@gmail.com>
2024-03-14 06:48:27 -0700

    W||K Tutorial: Add run, run_batched, and run_streamed
    Given a list of jobs, here are some basic ways to
    "decorate" the process of running the whole list.

--- src/schedulers/main.py
+++ src/schedulers/main.py
@@ -5,7 +5,11 @@ import asyncio
 
 # ==========
 
-# this code is inside the library
+# singular
+# these functions upgrade *individual* coroutines
+# * retry me on failure
+# * print before and after calling me
+# * these are *decorators for coroutine functions*
 
 async def retry(func, *args, **kwds):
     while True:
@@ -17,19 +21,55 @@ async def retry(func, *args, **kwds):
 
 # ==========
 
+# plural
+# these functions upgrade the process of running a *list* of coroutines
+# * run all at once
+# * run in batches and block until each is done
+
+async def run(jobs):
+    coros = [func(*args, **kwds) for (func, args, kwds) in jobs]
+    return await asyncio.gather(*coros)
+
+async def run_batched(jobs, n):
+    results = []
+    while jobs:
+        batch = jobs[:n]
+        coros = [func(*args, **kwds) for (func, args, kwds) in batch]
+        results += await asyncio.gather(*coros)
+        jobs = jobs[n:]
+    return results
+
+async def run_streamed(jobs, n):
+    results = []
+    sem = asyncio.Semaphore(n)
+    async def wrap(coro):
+        async with sem:
+            return await coro
+    while jobs:
+        batch = jobs[:n]
+        coros = [wrap(func(*args, **kwds)) for (func, args, kwds) in batch]
+        results += await asyncio.gather(*coros)
+        jobs = jobs[n:]
+    return results
+
+# ==========
+
 # this code is outside the library
 
 async def fail(n):
     """ sometimes tasks fail """
     import random
     await asyncio.sleep(0)
+    print(f"fail({n}): entering")
     if random.random() < 0.99:
+        print(f"fail({n}): failed")
         raise RuntimeError("no")
+    print(f"fail({n}): finished")
     return n
 
 async def main():
-    coros = [retry(fail, n) for n in range(100)]
-    return await asyncio.gather(*coros)
+    jobs = [(retry, (fail, n), {}) for n in range(100)]
+    return await run_streamed(jobs)
 
 results = asyncio.run(main())
 
