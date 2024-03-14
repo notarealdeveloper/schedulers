a6145cb3d159252188e81c7b5286eec8fc733313
Jason Wilkes <notarealdeveloper@gmail.com>
2024-03-14 09:43:34 -0700

    W||K Tutorial: Minimum working example of async rate-limited retry

--- src/schedulers/main.py
+++ src/schedulers/main.py
@@ -52,6 +52,32 @@ async def run_streamed(jobs, n):
         jobs = jobs[n:]
     return results
 
+async def run_rate_limited(jobs, jobs_per_sec):
+    results = []
+    import time
+    from collections import deque
+    times = deque([], maxlen=n)
+    sem = asyncio.Semaphore(n)
+    global_start = time.time()
+    num_done = 0
+    async def limiter(coro, k):
+        nonlocal num_done
+        async with sem:
+            result = await coro
+            num_done += 1
+            while True:
+                total_secs = time.time() - global_start
+                if (num_done < jobs_per_sec*total_secs):
+                    break
+                await asyncio.sleep(0.1)
+        return result
+    while jobs:
+        batch = jobs[:n]
+        coros = [limiter(func(*args, **kwds), k) for k, (func, args, kwds) in enumerate(batch)]
+        results += await asyncio.gather(*coros)
+        jobs = jobs[n:]
+    return results
+
 # ==========
 
 # this code is outside the library
@@ -83,15 +109,12 @@ async def main():
     return await run_batched(jobs, 10)
 
 async def main():
-    from slower import Slower
-    seconds = 1
-    async def wrap(coro, s):
-        await s.acquire()
-        return await coro
-    async with Slower(5, seconds) as s:
-        #jobs = [(retry, (fail, n), {}) for n in range(100)]
-        jobs = [wrap(fail(n), s) for n in range(100)]
-        return await asyncio.gather(*jobs)
+    jobs = [(retry, (fail, n), {}) for n in range(100)]
+    return await run_streamed(jobs, 10)
+
+async def main():
+    jobs = [(retry, (fail, n), {}) for n in range(100)]
+    return await run_rate_limited(jobs, 10)
 
 results = asyncio.run(main())
 
